9edf15207c1a84cd8e7e8a854ab917f9dd413012
andreas.engstrom <andreas.engstrom@taxistockholm.se>
2021-11-23 15:51:59 +0100

    ...

--- src/k8s/gcp/gcp-deploy.sh
+++ src/k8s/gcp/gcp-deploy.sh
@@ -5,18 +5,16 @@ ROOT_DIR=$(pwd)
 KUBECTL_DIR="$ROOT_DIR/kubectl"
 NAMESPACE_DIR="$KUBECTL_DIR/memes-namespace"
 CONFIG_DIR="$KUBECTL_DIR/memes-config"
+HELM_DIR="$ROOT_DIR/helm"
+HELM_NGINX_DIR="$HELM_DIR/ingress-nginx"
+HELM_DATABASE_DIR="$HELM_DIR/memes-database"
+HELM_MEMES_DIR="$HELM_DIR/memes-service"
+EXTERNAL_IP=$(gcloud compute addresses list --format="value(address)")
 
 # Create namespace, config and secret using kubectl
 kubectl apply -f ${NAMESPACE_DIR}/namespace.yaml
 kubectl apply -n memes -f ${CONFIG_DIR}/database-config.yaml
 kubectl create secret generic -n memes database-credentials --from-env-file ${CONFIG_DIR}/database-credentials.env
-
-HELM_DIR="$ROOT_DIR/helm"
-HELM_NGINX_DIR="$HELM_DIR/ingress-nginx"
-HELM_DATABASE_DIR="$HELM_DIR/memes-database"
-HELM_MEMES_DIR="$HELM_DIR/memes-service"
-
-EXTERNAL_IP=$(gcloud compute addresses list --format="value(address)")
 
 # Install an Nginx ingress controller using Helm3, show output and debug
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
@@ -27,7 +25,7 @@ helm3 upgrade --install ingress-nginx ingress-nginx/ingress-nginx -n ingress-ngi
 # Install a database using Helm3, show output and debug
 helm3 repo add bitnami https://charts.bitnami.com/bitnami
 helm3 repo update
-helm3 upgrade --install memes-database bitnami/postgresql -n memes -f ${HELM_DATABASE_DIR}/values-memes.yaml --wait
+helm3 upgrade --install memes-database bitnami/postgresql -n memes -f ${HELM_DATABASE_DIR}/values-memes-gcp.yaml --wait
 
 # Create the memes-service using Helm, show outputs
 helm3 upgrade --install memes-service ${HELM_MEMES_DIR} -n memes --wait
--- src/k8s/minikube/minikube-deploy.sh
+++ src/k8s/minikube/minikube-deploy.sh
@@ -2,6 +2,14 @@
 
 ROOT_DIR=$(pwd)
 MINIKUBE_DIR="${ROOT_DIR}/minikube"
+KUBECTL_DIR="$ROOT_DIR/kubectl"
+NAMESPACE_DIR="$KUBECTL_DIR/memes-namespace"
+CONFIG_DIR="$KUBECTL_DIR/memes-config"
+KUSTOMIZE_DIR="$ROOT_DIR/kustomize"
+KUSTOMIZE_MEMES_DIR="$KUSTOMIZE_DIR/memes-service"
+HELM_DIR="$ROOT_DIR/helm"
+HELM_DATABASE_DIR="$HELM_DIR/memes-database"
+HELM_MEMES_DIR="$HELM_DIR/memes-service"
 
 # Start and configure the Minikube
 minikube start --driver=hyperkit --memory 8192 --cpus 4
@@ -9,31 +17,25 @@ minikube addons enable ingress
 kubectl wait -n ingress-nginx --for=condition=Ready pod --field-selector=status.phase!=Succeeded --timeout=120s
 minikube addons enable gcp-auth
 
-KUBECTL_DIR="$ROOT_DIR/kubectl"
-NAMESPACE_DIR="$KUBECTL_DIR/memes-namespace"
-CONFIG_DIR="$KUBECTL_DIR/memes-config"
 
 # Create namespace, config and secret using kubectl
+cat ${NAMESPACE_DIR}/namespace.yaml
 kubectl apply -f ${NAMESPACE_DIR}/namespace.yaml
+
 minikube addons enable gcp-auth --refresh
+
+cat ${CONFIG_DIR}/database-config.yaml
 kubectl apply -n memes -f ${CONFIG_DIR}/database-config.yaml
+
+cat ${CONFIG_DIR}/database-credentials.env
 kubectl create secret generic -n memes database-credentials --from-env-file ${CONFIG_DIR}/database-credentials.env
 
 
-KUSTOMIZE_DIR="$ROOT_DIR/kustomize"
-KUSTOMIZE_MEMES_DIR="$KUSTOMIZE_DIR/memes-service"
-
 # Create the memes-service using Kustomize, show outputs
 # kustomize build ${KUSTOMIZE_MEMES_DIR}
-
 # kustomize build ${KUSTOMIZE_MEMES_DIR} | kubectl apply -f -
-
 # kustomize build ${KUSTOMIZE_MEMES_DIR} | kubectl delete -f -
 
-
-HELM_DIR="$ROOT_DIR/helm"
-HELM_DATABASE_DIR="$HELM_DIR/memes-database"
-HELM_MEMES_DIR="$HELM_DIR/memes-service"
 
 # Install a database using Helm3, show output and debug
 helm3 repo add bitnami https://charts.bitnami.com/bitnami
@@ -46,4 +48,4 @@ helm3 upgrade --install memes-database bitnami/postgresql -n memes -f ${HELM_DAT
 helm3 upgrade --install memes-service ${HELM_MEMES_DIR} -n memes -f ${HELM_MEMES_DIR}/values-dev.yaml --wait
 
 # Verify
-# http://memes.waymark.se/meme
+# http://memes-dev.waymark.se/meme
